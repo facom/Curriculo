0fc07cfc6ea72c7ffb16117b61ae506c31774be9
Jorge Zuluaga <zuluagajorge@gmail.com>
2015-05-28 12:08:58 -0500

    Commit

--- etc/publicbatch.py
+++ etc/publicbatch.py
@@ -14,16 +14,36 @@ courses=["0302100","0302108","0302164","0302251","0302252","0302261","0302262","
 ###################################################
 curriculo,connection=loadDatabase()
 db=connection.cursor()
+fields=curriculo["MicroCurriculos"]["fields"]
 
-print curriculo["MicroCurriculos"]["rows"]["0302100"]["F025_AUTH_Version"]
+codigo="0302150"
+semid="2002-1"
+curriculo["MicroCurriculos"]["rows"]["0302150"]["F025_AUTH_Version"]="1"
+version=int(curriculo["MicroCurriculos"]["rows"]["0302150"]["F025_AUTH_Version"])
+codigoid="%s-v%d-%s"%(codigo,version,semid)
+
+try:
+    curriculo["MicroCurriculos_Publicos"]["rows"][codigo]["F000_AUTO_Codigoid"]
+    print "El curso es público."
+except:
+    print "El curso no es público."
+    curriculo["MicroCurriculos_Publicos"]["rows"][codigo]=dict()
+    sql="insert into MicroCurriculos_Publicos (F000_AUTO_Codigoid) values (\"%s\") on duplicate key update F000_AUTO_Codigoid=\"%s\""%(codigoid,codigoid);    
+    db.execute(sql);
+    
+curriculo["MicroCurriculos_Publicos"]["rows"][codigo]["F000_AUTO_Codigoid"]=codigoid
+for field in fields:
+    value=curriculo["MicroCurriculos"]["rows"][codigo][field]
+    curriculo["MicroCurriculos_Publicos"]["rows"][codigo][field]=value
+
+print curriculo["MicroCurriculos_Publicos"]["rows"][codigo]
 exit(0)
-
 
 for year in years:
     print "Year: ",year
     for sem in sems:
         semid=str(year)+"-"+str(sem)
         print "\tSemester: ",sem,semid
-        for course in courses:
+        for codigo in courses:
             print "\t\tCourse: ",course
             
